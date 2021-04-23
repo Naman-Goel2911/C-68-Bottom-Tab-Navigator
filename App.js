05a3705031b34b5a4aa5171b724ac086b2799f06
Naman Goel <vakulg@gmail.com>
2021-04-23 16:54:55 +0530

    Added Bottom Tabs

--- App.js
+++ App.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import {Text, View } from 'react-native';
 import {createAppContainer} from 'react-navigation';
-import {createBottomNavigator} from 'react-navigation-tabs';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import fb from './screens/fb'
 import insta from './screens/insta'
 import {Header} from 'react-native-elements'
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 
 export default class App extends React.Component{
   render()
   {
     return(
-      <View>
-        <Header 
-        backgroundColor={'#9c8210'}
-        centerComponent={{
-          text: 'Bottom tab Navigator',
-          style: { color: '#fff', fontSize: 20 },
-        }}/>
-        <AppContainer />
-      </View>
+      <SafeAreaProvider>
+        <View>
+          <Header 
+          backgroundColor={'#9c8210'}
+          centerComponent={{
+            text: 'Bottom tab Navigator',
+            style: { color: '#fff', fontSize: 20 },
+          }}/>
+          <AppContainer />
+        </View>
+      </SafeAreaProvider>
     )
   }
 }
 
-const tabNavigator = createBottomNavigator({
+const tabNavigator = createBottomTabNavigator({
   Facebook: fb,
   Instagram: insta
 })
